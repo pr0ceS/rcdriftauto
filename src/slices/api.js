265bcda140363ab0705b141ed9104b4475a82680
Mehmet <xxmkbozzzxx@gmail.com>
2023-08-09 00:50:39 +0200

    api.js fix

--- src/slices/api.js
+++ src/slices/api.js
@@ -1,4 +1,4 @@
-export const url = "https://rcdriftautobackend.onrender.com/api/";
+export const url = "https://rcdriftautobackend.onrender.com/api";
 
 export const setHeaders = () => {
   const headers = {
